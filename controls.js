043db2eafd7eb08596b596ebd66bd56eec630183
Glitch (pitch-gaudy-acai) <none>
2020-07-05 08:25:07 +0000

    rotation from keyboard keys: YGHJ

--- controls.js
+++ controls.js
@@ -8,24 +8,39 @@ function toggleColor() {
 }
 
 AFRAME.registerComponent("rig-thrusters", {
+  schema: {    
+    forward: { type: 'number', default: 0 },
+    right: { type: 'number', default: 0 },
+    turnDown: { type: 'number', default: 0 },
+    turnRight: { type: 'number', default: 0 },
+  },
   init: function() {
     this.directionVec3 = new THREE.Vector3();
+    this.rotationVec3 = new THREE.Vector3();
   },
   tick: function() {
     ["x", "y", "z"].forEach(axis => {
       this.el.object3D.position[axis] += this.directionVec3[axis];
+      this.el.object3D.rotation[axis] += this.rotationVec3[axis];
     });
     // console.log("rig tick => camera:", this.el.object3D.position);
   },
   update: function(oldData) {
-    const { forward, right } = this.data;
+    const { forward, right, turnDown, turnRight } = this.data;
     if (right !== undefined) {
       this.directionVec3.x = right / 10;
     }
     if (forward !== undefined) {
       this.directionVec3.z = forward / 10;
     }
+    if (turnDown !== undefined) {
+      this.rotationVec3.x = turnDown / 10;
+    }
+    if (turnRight !== undefined) {
+      this.rotationVec3.y = turnRight / 10;
+    }
     console.log("rig update => direction:", this.directionVec3);
+    console.log("rig update => rotation:", this.rotationVec3);
   }
 });
 
@@ -34,34 +49,56 @@ AFRAME.registerComponent("keyboard-control", {
 
   init: function() {
     const rigElementId = this.data;
-    if (!rigElementId) {
-      console.warn(
-        "rig element id must be provided to keyboard-control component"
-      );
-      return;
-    }
-
     const rig = document.getElementById(rigElementId);
+
+    // Keyboard controls:
+    //
+    //      W           Y
+    //    A S D       G H J
+    // translation  rotation
 
     window.addEventListener("keydown", function(event) {
       switch (event.key) {
+        // translation keys
         case "w":
-          rig.setAttribute("rig-thrusters", "forward", -1);
-          break;
         case "s":
-          rig.setAttribute("rig-thrusters", "forward", 1);
+          rig.setAttribute(
+            "rig-thrusters",
+            "forward",
+            event.key === "w" ? -1 : 1
+          );
           break;
         case "a":
-          rig.setAttribute("rig-thrusters", "right", -1);
-          break;
         case "d":
-          rig.setAttribute("rig-thrusters", "right", 1);
+          rig.setAttribute(
+            "rig-thrusters",
+            "right",
+            event.key === "a" ? -1 : 1
+          );
+          break;
+        // rotation keys
+        case "y":
+        case "h":
+          rig.setAttribute(
+            "rig-thrusters",
+            "turnDown",
+            event.key === "y" ? -1 : 1
+          );
+          break;
+        case "g":
+        case "j":
+          rig.setAttribute(
+            "rig-thrusters",
+            "turnRight",
+            event.key === "g" ? 1 : -1
+          );
           break;
       }
     });
 
     window.addEventListener("keyup", function(event) {
       switch (event.key) {
+        // translation keys
         case "w":
         case "s":
           rig.setAttribute("rig-thrusters", "forward", 0);
@@ -69,6 +106,15 @@ AFRAME.registerComponent("keyboard-control", {
         case "a":
         case "d":
           rig.setAttribute("rig-thrusters", "right", 0);
+          break;
+        // rotation keys
+        case "y":
+        case "h":
+          rig.setAttribute("rig-thrusters", "turnDown", 0);
+          break;
+        case "g":
+        case "j":
+          rig.setAttribute("rig-thrusters", "turnRight", 0);
           break;
       }
     });
@@ -80,13 +126,6 @@ AFRAME.registerComponent("rig-movement-controller", {
 
   init: function() {
     const rigElementId = this.data;
-    if (!rigElementId) {
-      console.warn(
-        "rig element id must be provided to rig-movement-controller component"
-      );
-      return;
-    }
-
     const rig = document.getElementById(rigElementId);
 
     // https://aframe.io/docs/1.0.0/components/tracked-controls.html
